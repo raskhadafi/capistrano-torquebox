91d755a9e3602ef49d991ff5b8ed31fdfd12a720
Roman Simecek <roman@good2go.ch>
2013-12-30 10:05:22 +0100

    Split into two loadable recipes.

--- lib/capistrano/torquebox.rb
+++ lib/capistrano/torquebox.rb
@@ -1,1 +1,2 @@
-load File.expand_path('../tasks/torquebox.rake', __FILE__)
+load File.expand_path('../tasks/torquebox/load.rake', __FILE__)
+load File.expand_path('../tasks/torquebox/deploy.rake', __FILE__)
---
+++ lib/capistrano/torquebox/load.rb
@@ -0,0 +1,1 @@
+load File.expand_path('../../tasks/torquebox/load.rake', __FILE__)
